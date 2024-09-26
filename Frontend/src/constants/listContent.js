544b14f5ac90710f95313a87122a370b65a1e816
GlennWilliam <glennwilliam1009@gmail.com>
2024-09-26 00:23:17 -0700

    Update constants list and add new assets

---
+++ Frontend/src/constants/listContent.js
@@ -0,0 +1,66 @@
+const LIST_CONTENT_1_EN = [
+  {
+    title: "Enjoy on your TV",
+    desc: "Watch on Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+  },
+];
+
+const LIST_CONTENT_2_EN = [
+  {
+    title: "Download your shows to watch offline",
+    desc: "Save your favorites easily and always have something to watch.",
+  },
+];
+
+const LIST_CONTENT_3_EN = [
+  {
+    title: "Watch everywhere",
+    desc: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+  },
+];
+
+const LIST_CONTENT_4_EN = [
+  {
+    title: "Create profile for kids",
+    desc: "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
+  },
+];
+
+const LIST_CONTENT_1_ID = [
+  {
+    title: "Nikmati di TV-mu",
+    desc: "Tonton di Smart TV, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players, dan sebagainya.",
+  },
+];
+
+const LIST_CONTENT_2_ID = [
+  {
+    title: "Download acara TV untuk menontonnya secara offline",
+    desc: "Simpan favoritmu dengan mudah agar selalu ada acara TV dan film yang bisa ditonton",
+  },
+];
+
+const LIST_CONTENT_3_ID = [
+  {
+    title: "Tonton di mana saja",
+    desc: "Streaming film dan acara TV tak terbatas di ponsel, tablet, laptop, dan TV-mu",
+  },
+];
+
+const LIST_CONTENT_4_ID = [
+  {
+    title: "Buat profil untuk anak-anak",
+    desc: "Kirim anak-anak untuk bertualang bersama karakter favorit mereka di dunia yang dibuat khusus untuk mereka-gratis dengan keanggotaan",
+  },
+];
+
+export {
+    LIST_CONTENT_1_EN,
+    LIST_CONTENT_2_EN,
+    LIST_CONTENT_3_EN,
+    LIST_CONTENT_4_EN,
+    LIST_CONTENT_1_ID,
+    LIST_CONTENT_2_ID,
+    LIST_CONTENT_3_ID,
+    LIST_CONTENT_4_ID
+}
